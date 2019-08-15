0b1069e372a71f1d17b2d9aa17d06a9eb7e9f4a3
Moritz Schmidt <mo.schmidt@rebuy.com>
2019-08-15 22:22:33 +0200

    aaaand it works. for events.

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import {createServer, IncomingMessage, ServerResponse} from "http"
-import {readFileSync} from "fs"
+import {existsSync, readFileSync} from 'fs'
 import {gunzip} from "zlib"
 
 const io = require("java.io")
@@ -8,34 +8,53 @@ const InputObjectStream = io.InputObjectStream
 interface Config {
     repositoryPath: string
     port: number
+    pretty: boolean
 }
 
 const config = JSON.parse(readFileSync(__dirname + "/config.json").toString()) as Config
 
 if (config.repositoryPath.endsWith('/')) {
-    config.repositoryPath = config.repositoryPath.substr(-1)
+    config.repositoryPath = config.repositoryPath.substr(0, -1)
 }
 if (config.repositoryPath.endsWith('.a3s')) {
-    config.repositoryPath = config.repositoryPath.substr(-4)
+    config.repositoryPath = config.repositoryPath.substr(0, -4)
 }
 
 function getFullFilePath(httpPath: string): string {
     let [fileName, directory] = httpPath.split('/').reverse()
-    if (directory === ".a3s") {
-        return `${config.repositoryPath}/${directory}/${fileName}`
+
+    console.log(fileName)
+    console.log(directory)
+    if (directory === ".a3s" && fileName.endsWith(".json") && directory.indexOf("..") === -1) {
+        return `${config.repositoryPath}/${directory}/${fileName.split('.').reverse().slice(1).reverse().join('.')}`
     }
-    throw new Error("something's off here")
+
+    throw new Error(`something's off here: httpPath=${httpPath}, repositoryPath=${config.repositoryPath}`)
 }
 
 createServer({}, (req: IncomingMessage, res: ServerResponse) => {
-    const filename = getFullFilePath(req.url)
+    let filename
+    try {
+        filename = getFullFilePath(req.url)
+    } catch (e) {
+        res.writeHead(400);
+        res.end(e.message);
+        return;
+    }
+    if (!existsSync(filename)) {
+        res.writeHead(404);
+        res.write(`file ${filename} not found`)
+        res.end();
+        return;
+    }
+    console.info(`will try to read ${filename}`)
     const buf = readFileSync(filename);
     console.debug(`buf length: ${buf.length} for file ${filename}`)
     gunzip(buf, (err,data) => {
         const inStream = new InputObjectStream(data);
         const obj = inStream.readObject();
 
-        res.write(JSON.stringify(obj))
+        res.write(JSON.stringify(obj, null, config.pretty ? "\t" : undefined))
         res.end()
     })
 }).listen(config.port)
